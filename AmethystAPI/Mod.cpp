2655a2b69239b74401584e3a11235ad963e06d24
FrederoxDev <blakefreddie96@gmail.com>
2023-10-01 16:03:15 +0100

    Upd: README && appendFormattedHoverText POC

--- AmethystAPI/Mod.cpp
+++ AmethystAPI/Mod.cpp
@@ -5,20 +5,21 @@ typedef void(__thiscall* _Item_Item)(void*, const std::string&, short);
 _Item_Item _item_Item;
 
 static void Item_Item(void* self, const std::string& identifier, short id) {
-    //Log::Info("Item::Item({}, {})\n", identifier, id);
     _item_Item(self, identifier, id);
 }
 
-// Item::setMaxStackSize
-typedef void(__thiscall* _Item_setMaxStackSize)(void*, unsigned char);
-_Item_setMaxStackSize _item_setMaxStackSize;
+class ItemStackBase;
+class Level;
 
-void Item_setMaxStackSize(void* self, unsigned char size) {
-    //Log::Info("Item::setMaxStackSize({})\n", size);
-    _item_setMaxStackSize(self, 13);
+typedef void(__thiscall* _Item_appendFormattedHoverText)(void*, const ItemStackBase&, Level&, std::string&, bool);
+_Item_appendFormattedHoverText _item_appendFormattedHoverText;
+
+void Item_appendFormattedHoverText(void* self, const ItemStackBase& isb, Level& lvl, std::string& text, bool b) {
+    _item_appendFormattedHoverText(self, isb, lvl, text, b);
+    text = "Hello, World!";
 }
 
 void ModInitializeHooks() {
-    CreateHook(SlideAddress(0x3211fa0), Item_setMaxStackSize, _item_setMaxStackSize);
     CreateHook(SlideAddress(0x3210ec0), Item_Item, _item_Item);
+    CreateHook(SlideAddress(0x3215CD0), Item_appendFormattedHoverText, _item_appendFormattedHoverText);
 }
--- README.md
+++ README.md
@@ -5,3 +5,36 @@
  - [ ] Sig-scanning
  - [ ] Build into a specific %amethyst% folder
  - [ ] Load mods at runtime / support multiple mods
+
+# Examples
+### Hooking a constructor
+```cpp
+typedef void(__thiscall* _Item_Item)(void*, const std::string&, short);
+_Item_Item _item_Item;
+
+static void Item_Item(void* self, const std::string& identifier, short id) {
+    _item_Item(self, identifier, id);
+}
+
+void ModInitializeHooks() {
+    CreateHook(SlideAddress(0x3210ec0), Item_Item, _item_Item);
+}
+```
+
+### Hooking a virtual function
+```cpp
+class ItemStackBase;
+class Level;
+
+typedef void(__thiscall* _Item_appendFormattedHoverText)(void*, const ItemStackBase&, Level&, std::string&, bool);
+_Item_appendFormattedHoverText _item_appendFormattedHoverText;
+
+void Item_appendFormattedHoverText(void* self, const ItemStackBase& isb, Level& lvl, std::string& text, bool b) {
+    _item_appendFormattedHoverText(self, isb, lvl, text, b);
+    text = "Hello, World!";
+}
+
+void ModInitializeHooks() {
+    CreateHook(SlideAddress(0x3215CD0), Item_appendFormattedHoverText, _item_appendFormattedHoverText);
+}
+```
