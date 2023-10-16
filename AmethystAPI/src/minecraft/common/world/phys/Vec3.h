15a595cda0a1312db85c4deb181dd8cd23834a6a
FrederoxDev <blakefreddie96@gmail.com>
2023-10-16 21:06:32 +0100

    Add Vec3.h

---
+++ AmethystAPI/src/minecraft/common/world/phys/Vec3.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Vec3 {
+public:
+	float x;
+	float y;
+	float z;
+
+public:
+	Vec3(float x, float y, float z);
+
+	Vec3 operator+(const Vec3 rhs);
+	Vec3 operator-(const Vec3 rhs);
+	Vec3 operator*(const Vec3 rhs);
+	Vec3 operator/(const Vec3 rhs);
+	bool operator==(const Vec3 rhs);
+};
