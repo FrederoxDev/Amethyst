3013735181d2c4d818704d50e83eae69c1d683c2
FrederoxDev <blakefreddie96@gmail.com>
2024-01-01 12:46:56 +0000

    VanillaClientInputMappingFactory::__createBaseNormalGamePlayKeyboardAndMouseMapping

---
+++ AmethystAPI/src/minecraft/src-client/common/client/input/VanillaClientInputMappingFactory.cpp
@@ -0,0 +1,5 @@
+#include "src-client/common/client/input/VanillaClientInputMappingFactory.h"
+
+// void* VanillaClientInputMappingFactory::_bindActionToKeyboardAndMouseInput(void* a1, void* a2, void* a3, void* a4, void* a5, char a6) {
+
+// }
