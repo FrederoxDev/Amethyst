c4ca9b6a1f007c3200df28f0cf1949412aed84cd
ATXLtheAxolotl <flashtube107@gmail.com>
2024-01-07 20:07:06 -0500

    fix(): Oops this should be right now

--- AmethystRuntime/src/AmethystRuntime.cpp
+++ AmethystRuntime/src/AmethystRuntime.cpp
@@ -57,8 +57,8 @@ void AmethystRuntime::LoadModDlls()
         _LoadModFunc(&mModStartJoinGame, mod, "OnStartJoinGame");
         _LoadModFunc(&mModRender, mod, "OnRenderUI");
         _LoadModFunc(&mModShutdown, mod, "Shutdown");
-        _LoadModFunc(&mModTickBefore, mod, "TickBefore");
-        _LoadModFunc(&mModTickAfter, mod, "TickAfter");
+        _LoadModFunc(&mModTickBefore, mod, "BeforeTick");
+        _LoadModFunc(&mModTickAfter, mod, "AfterTick");
     }
 }
 
