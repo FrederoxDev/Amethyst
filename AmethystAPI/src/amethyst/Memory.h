fdd49b061a053581577704700e4b4ba4483eb8ce
chyves <82107846+notchyves@users.noreply.github.com>
2024-01-16 20:39:22 -0500

    build_access

--- AmethystAPI/src/amethyst/Memory.h
+++ AmethystAPI/src/amethyst/Memory.h
@@ -8,6 +8,30 @@
 #include <vector>
 #include <windows.h>
 
+template <typename Ret, typename Type>
+Ret& direct_access(Type* type, size_t offset) {
+    union {
+        size_t raw;
+        Type* source;
+        Ret* target;
+    } u;
+    u.source = type;
+    u.raw += offset;
+    return *u.target;
+}
+
+#define AS_FIELD(type, name, fn) __declspec(property(get = fn, put = set##name)) type name
+#define DEF_FIELD_RW(type, name) __declspec(property(get = get##name, put = set##name)) type name
+
+#define FAKE_FIELD(type, name)       \
+	AS_FIELD(type, name, get##name); \
+	type get##name()
+
+#define BUILD_ACCESS(ptr, type, name, offset)                           \
+	AS_FIELD(type, name, get##name);                                    \
+	type get##name() const { return direct_access<type>(ptr, offset); } \
+	void set##name(type v) const { direct_access<type>(ptr, offset) = v; }
+
 /*
 Returns the position where Minecraft has been loaded into memory
 */
