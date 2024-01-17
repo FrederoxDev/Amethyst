fd5ceaf5bd2c177bafb056d90215334b2ffa6b39
chyves <82107846+notchyves@users.noreply.github.com>
2024-01-16 20:51:49 -0500

    add layername stuff
    basically screenview->visualtree->root->layername

--- AmethystAPI/src/minecraft/src-client/common/client/gui/ScreenView.h
+++ AmethystAPI/src/minecraft/src-client/common/client/gui/ScreenView.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "minecraft/src-client/common/client/renderer/screen/MinecraftUIRenderContext.h"
+#include "minecraft/src-client/common/client/gui/gui/VisualTree.h"
 
 // Not sure if this is correct..
 using UIRenderContext = MinecraftUIRenderContext;
 
 class ScreenView {
 public:
+
+    BUILD_ACCESS(this, class VisualTree*, VisualTree, 0x48);
+
     // 48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B FA 48 89 54 24 ? 4C 8B E9 48 89 4C 24
     typedef void*(__thiscall* _setupAndRender)(ScreenView*, UIRenderContext*);
     void* setupAndRender(UIRenderContext*);
