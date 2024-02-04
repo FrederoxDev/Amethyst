cd1061fc1534c43dc1290ef5c01be80c81f56914
ATXLtheAxolotl <flashtube107@gmail.com>
2024-02-03 19:30:56 -0500

    fix(): Prevent crash when assigning default input maps

--- AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.h
+++ AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.h
@@ -3,9 +3,9 @@
 
 class LevelRendererPlayer {
 public:
+    //48 8B C4 48 89 58 ? 48 89 70 ? 57 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 48 8B 05
     virtual float getFov(float, bool);
 
 public:
-    //48 8B C4 48 89 58 ? 48 89 70 ? 57 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 48 8B 05
-    typedef float(__fastcall* _getFov)(LevelRendererPlayer*, float, bool);
+    typedef float(__thiscall* _getFov)(LevelRendererPlayer*, float, bool);
 };
