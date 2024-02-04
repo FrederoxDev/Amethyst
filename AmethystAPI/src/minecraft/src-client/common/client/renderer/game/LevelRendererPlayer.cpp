a2d72c95435b24f66f67d1107d2e1f502eaef4bd
ATXLtheAxolotl <flashtube107@gmail.com>
2024-02-04 08:19:57 -0500

    fix(): Rm unecessary typedef

--- AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.cpp
+++ AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.cpp
@@ -1,6 +1,7 @@
 #include "minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.h"
 
 float LevelRendererPlayer::getFov(float originalFov, bool applyEffects) {
-    static auto func = reinterpret_cast<LevelRendererPlayer::_getFov>(SigScan("48 8B C4 48 89 58 ? 48 89 70 ? 57 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 48 8B 05"));
+    using function = float(__thiscall*)(LevelRendererPlayer*, float, bool);
+    static auto func = reinterpret_cast<function>(SigScan("48 8B C4 48 89 58 ? 48 89 70 ? 57 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 48 8B 05"));
     return func(this, originalFov, applyEffects);
 }
--- AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.h
+++ AmethystAPI/src/minecraft/src-client/common/client/renderer/game/LevelRendererPlayer.h
@@ -5,7 +5,4 @@ class LevelRendererPlayer {
 public:
     //48 8B C4 48 89 58 ? 48 89 70 ? 57 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 48 8B 05
     virtual float getFov(float, bool);
-
-public:
-    typedef float(__thiscall* _getFov)(LevelRendererPlayer*, float, bool);
 };
