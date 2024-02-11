e36dc16169d563c20c4f355561b73cb8626d510f
FrederoxDev <blakefreddie96@gmail.com>
2024-02-11 12:35:04 +0000

    Implement BlockTypeRegistry::registerBlock

--- AmethystAPI/src/minecraft/src/common/world/level/block/registry/BlockTypeRegistry.h
+++ AmethystAPI/src/minecraft/src/common/world/level/block/registry/BlockTypeRegistry.h
@@ -6,6 +6,7 @@
 #include "minecraft/src/common/world/level/block/BlockLegacy.h"
 #include "minecraft/src/common/SharedPtr.h"
 #include "entt/container/dense_map.hpp"
+#include "minecraft/src-deps/core/utility/StringUtils.h"
 
 typedef std::map<HashedString, SharedPtr<BlockLegacy>> BlockLookupMap;
 typedef entt::dense_map<unsigned long, HashedString> BlockNameHashToHashedStringMap;
@@ -31,5 +32,47 @@ public:
     static BlockNameHashToHashedStringMap* mBlockNameHashToStringMap() {
         static auto* map = reinterpret_cast<BlockNameHashToHashedStringMap*>(SlideAddress(0x551F010));
         return map;
+    }
+
+    // Re-Implemented
+    template<typename T, typename... Args>
+    static WeakPtr<T> registerBlock(const std::string& blockName, Args&&... args) {
+        HashedString hashedBlockName(blockName);
+
+        // Ensure the block has a name
+        if (blockName.empty()) {
+            throw std::exception("BlockTypeRegistry: attempting to register a block without a name!");
+        }
+
+        // BlockTypeRegistry::lockForRegistryModifications();
+
+        SharedPtr<T> block = SharedPtr<T>::make(blockName, std::forward<Args>(args)...);
+
+        // Convert the string to lowercase and validate its namespace
+        std::string lowercaseName = Util::toLower(blockName);
+        size_t separator = lowercaseName.find(':');
+
+        if (separator == std::string::npos) {
+            throw std::exception("BlockTypeRegistry: attempting to register a block without a namespace!");
+        }
+
+        if (separator == 0) {
+            throw std::exception("BlockTypeRegistry: attempting to register a block with an empty namespace!");
+        }
+
+        // Add the namespace to a set of known namespaces
+        std::string blockNamespace = lowercaseName.substr(0, separator);
+        std::set<std::string>* namespaceSet = BlockTypeRegistry::mKnownNamespaces();
+        namespaceSet->insert(blockNamespace);
+
+        // Add the block to the lookup map
+        BlockLookupMap* blockLookupMap = BlockTypeRegistry::mBlockLookupMap();
+        blockLookupMap->emplace(std::make_pair(hashedBlockName, block));
+
+        // Add the hashed name to a lookup map
+        BlockNameHashToHashedStringMap* hashedNameMap = BlockTypeRegistry::mBlockNameHashToStringMap();
+        hashedNameMap->insert(std::make_pair((unsigned long)hashedBlockName.getHash(), hashedBlockName));
+
+        return block;
     }
 };
