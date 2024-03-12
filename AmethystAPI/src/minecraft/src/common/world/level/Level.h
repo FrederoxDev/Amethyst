7e996a31587da94b7441cbee185318a6a8e04c89
FrederoxDev <blakefreddie96@gmail.com>
2024-03-12 23:11:57 +0000

    Item & BlockType Registries in Level

--- AmethystAPI/src/minecraft/src/common/world/level/Level.h
+++ AmethystAPI/src/minecraft/src/common/world/level/Level.h
@@ -1,5 +1,16 @@
 #pragma once
 
-class Level {
+#include "minecraft/src/common/SharedPtr.h"
+#include <memory>
+#include <minecraft/src/common/world/item/registry/ItemRegistryRef.h>
 
+class BlockTypeRegistry;
+
+class Level {
+public:
+    /* this + 0    */ uintptr_t** vtable;
+    /* this + 8    */ std::byte padding8[340];
+    /* this + 348  */ const ItemRegistryRef mItemRegistry;
+    /* this + 364  */ std::weak_ptr<BlockTypeRegistry> mBlockRegistry;
+    /* this + 380  */ std::byte padding380[8620];
 };
