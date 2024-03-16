0be742f44b828da2586fbfbb3b934d5b13cc7edf
FrederoxDev <blakefreddie96@gmail.com>
2024-03-16 17:49:32 +0000

    Push the other stacks

--- AmethystAPI/src/minecraft/src-deps/renderer/Camera.h
+++ AmethystAPI/src/minecraft/src-deps/renderer/Camera.h
@@ -6,7 +6,8 @@ namespace mce {
     class Camera {
     public:
         /* this + 0   */ MatrixStack viewMatrixStack;
-        /* this + 64  */ std::byte padding64[128];
+        /* this + 64  */ MatrixStack worldMatrixStack;
+        /* this + 128 */ MatrixStack projectionMatrixStack;
         /* this + 192 */ glm::tmat4x4<float> mInverseViewMatrix;
         /* this + 256 */ std::byte padding256[256];
     };
