0b49cc416f094240c5eb0bb46b692daa372c74c3
Duckos-Mods <businwes@hotmail.com>
2024-03-17 12:03:39 +0000

    Implemented an (UNTESTED) byte patcher

---
+++ AmethystAPI/src/amethyst/runtime/patchManager/PatchManager.cpp
@@ -0,0 +1,76 @@
+#include "PatchManager.h"
+#include "../../Log.h"
+#include "../../Memory.h"
+namespace Amethyst {
+    // Returns false if the patch failed to apply
+    bool PatchManager::ApplyPatch(PatchAddress address, uint8_t* patch, size_t size) {
+        address = (void*)(GetMinecraftBaseAddress() + (uint64_t)address);
+        DWORD oldProtection;
+        if (!UnprotectMemory(address, size, &oldProtection)) {
+            Log::Error("Failed to unprotect memory at address: 0x{0:x}", address);
+			return false;
+		}
+        Patch ptch;
+        auto it = this->m_Patches.find(address);
+        if (it != this->m_Patches.end()) [[unlikely]] {
+            if (memcmp(it->second.address, patch, size)) {
+                Log::Warning("Patch already applied at address: 0x{0:x}", address);
+                ptch.original = it->second.original;
+            }
+		}
+        if (ptch.original.original == nullptr) {
+			OriginalMemory original;
+			uint8_t* originalMemory = (uint8_t*)malloc(size);
+			memcpy(originalMemory, address, size);
+			original.original = originalMemory;
+            original.size = size;
+			ptch.original = original;
+        }
+        ptch.address = address;
+        memcpy(address, patch, size);
+        this->m_Patches[address] = ptch;
+
+        if (!ProtectMemory(address, size, oldProtection)) {
+            Log::Error("Failed to protect memory at address: 0x{0:x}", address);
+            return false;
+        }
+		return true;
+
+    }
+
+    void PatchManager::RemovePatch(PatchAddress address)
+    {
+        auto it = this->m_Patches.find(address);
+		if (it != this->m_Patches.end()) {
+			DWORD oldProtection;
+			if (!UnprotectMemory(address, it->second.original.size, &oldProtection)) {
+				Log::Error("Failed to unprotect memory at address: 0x{0:x}", address);
+				return;
+			}
+			memcpy(address, it->second.original.original, it->second.original.size);
+			if (!ProtectMemory(address, it->second.original.size, oldProtection)) {
+				Log::Error("Failed to protect memory at address: 0x{0:x}", address);
+				return;
+			}
+			free(it->second.original.original);
+			this->m_Patches.erase(it);
+		}
+
+    }
+
+    void PatchManager::RemoveAllPatches()
+    {
+		for (auto it = this->m_Patches.begin(); it != this->m_Patches.end(); it++) {
+            RemovePatch(it->first);
+		}
+		this->m_Patches.clear();
+    }
+
+    bool PatchManager::UnprotectMemory(PatchAddress address, size_t size, DWORD* oldProtection) {
+        return VirtualProtect(address, size, PAGE_EXECUTE_READWRITE, oldProtection);
+    }
+    bool PatchManager::ProtectMemory(PatchAddress address, size_t size, DWORD oldProtection) {
+        DWORD oldOldProtection; // Only exists so windows doesnt have a hissy fit
+        return VirtualProtect(address, size, oldProtection, &oldOldProtection);
+    }
+} // namespace Amethyst
---
+++ AmethystAPI/src/amethyst/runtime/patchManager/PatchManager.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cstdint>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <unordered_map>
+
+using PatchAddress = void*;
+
+namespace Amethyst {
+	class PatchManager {
+	public:
+		PatchManager() {}
+		~PatchManager() {}
+		
+		template<typename T>
+        bool ApplyPatch(PatchAddress address, T patch){
+			return ApplyPatch(address, (uint8_t*)&patch, sizeof(T));
+		}
+        bool ApplyPatch(PatchAddress address, uint8_t* patch, size_t size);
+
+		void RemovePatch(PatchAddress address);
+
+		void RemoveAllPatches();
+	private:
+         bool UnprotectMemory(PatchAddress address, size_t size, DWORD* oldProtection);
+         bool ProtectMemory(PatchAddress address, size_t size, DWORD oldProtection);
+
+    private:
+		struct OriginalMemory {
+			uint8_t* original = nullptr;
+			size_t size = 0;
+		};
+		struct Patch {
+            PatchAddress address = 0x0;
+            OriginalMemory original = {};
+		};
+		std::unordered_map<PatchAddress, Patch> m_Patches; // Tbh a vector would be faster because i dont expect a lot of patches
+
+
+	};
+}
