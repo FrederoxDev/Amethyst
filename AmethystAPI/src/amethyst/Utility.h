585c7461ee9ec04d0ce891dcb99f435e87803e83
Adrian <adrian.olbrich1@gmail.com>
2024-03-24 15:02:08 +0100

    fixed not broken comment, everything is alright

--- AmethystAPI/src/amethyst/Utility.h
+++ AmethystAPI/src/amethyst/Utility.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
 
-/// Returns the path to the amethyst UWP folder
+/**
+ * Returns the path to the amethyst UWP folder
+ */
 std::string GetAmethystFolder();
 
 std::string StringFromWstring(std::wstring wstring);
