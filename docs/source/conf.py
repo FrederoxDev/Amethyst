c759f53f389b4ed3913d802591197d70bb06ef79
Adrian <adrian.olbrich1@gmail.com>
2024-04-03 17:41:09 +0200

    changed theme to sphinxawesome

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ release = ''
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.githubpages',
+    'sphinxawesome_theme.highlighting',
     'breathe'
 ]
 
@@ -26,7 +27,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
 
 # Breathe configuration
