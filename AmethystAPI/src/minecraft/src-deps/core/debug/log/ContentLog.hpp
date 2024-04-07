64e41a63975853912e9f2730a28a2aa4e8c03d78
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-04-07 10:59:25 -0500

    Remove ContentLog ctor body

--- AmethystAPI/src/minecraft/src-deps/core/debug/log/ContentLog.hpp
+++ AmethystAPI/src/minecraft/src-deps/core/debug/log/ContentLog.hpp
@@ -8,7 +8,7 @@ public:
     uintptr_t** vtable;
 
     // 1.20.72.1 - 48 89 5C 24 ? 48 89 4C 24 ? 57 48 83 EC ? 48 8B F9 48 8D 05 ? ? ? ? 48 89 01 48 89 4C 24 ? 48 83 C1 ? 48 8D 54 24 ? E8 ? ? ? ? 90 48 8D 05 ? ? ? ? 48 89 07 C6 47
-    ContentLog() {};
+    ContentLog();
 
     template<typename... Strings>
     int Log(bool doNotRepeat, LogLevel logLevel, LogArea logArea, Strings... strings) {
