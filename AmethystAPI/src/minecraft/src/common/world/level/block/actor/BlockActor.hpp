6250a1a64a4f7f3be5e370de60078231d021765a
FrederoxDev <blakefreddie96@gmail.com>
2024-05-23 12:08:37 +0100

    Add BlockActor class

---
+++ AmethystAPI/src/minecraft/src/common/world/level/block/actor/BlockActor.hpp
@@ -0,0 +1,98 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <minecraft/src/common/world/phys/AABB.hpp>
+#include <minecraft/src-deps/core/utility/NonOwnerPointer.hpp>
+#include <minecraft/src/common/world/level/BlockPos.hpp>
+#include <minecraft/src/common/world/level/block/Block.hpp>
+#include <minecraft/src/common/world/actor/ActorTerrainInterlockData.hpp>
+#include <minecraft/src/common/world/level/block/actor/BlockActorRendererId.hpp>
+#include <gsl/gsl>
+
+// Auto-generated: Unknown complete types
+class UIProfanityContext {}; 
+class BlockActorDataPacket {}; 
+
+// Auto-generated: Forward declarations
+class Level; 
+class CompoundTag; 
+class DataLoadHelper; 
+class BlockSource; 
+class BlockActor; 
+class BlockPos; 
+class IConstBlockSource; 
+class Player; 
+
+class BlockActor {
+public:
+    /* this + 8   */ int mTickCount;
+    /* this + 16  */ const Block* mBlock;
+    /* this + 24  */ float mDestroyTimer;
+    /* this + 28  */ Vec3 mDestroyDirection;
+    /* this + 40  */ float mDestroyProgress;
+    /* this + 44  */ BlockPos mPosition;
+    /* this + 56  */ AABB mBB;
+    /* this + 80  */ const BlockActorType mType;
+    /* this + 84  */ BlockActorRendererId mRendererId;
+    /* this + 88  */ std::string mCustomName;
+    /* this + 120 */ std::string mFilteredCustomName;
+    /* this + 152 */ int mRepairCost;
+    /* this + 156 */ bool mClientSideOnly;
+    /* this + 157 */ bool mIsMovable;
+    /* this + 158 */ bool mSaveCustomName;
+    /* this + 159 */ bool mCanRenderCustomName;
+    /* this + 160 */ const float signShadowRadius;
+    /* this + 168 */ ActorTerrainInterlockData mTerrainInterlockData;
+    /* this + 192 */ bool mChanged;
+
+public:
+	virtual ~BlockActor();
+	virtual void load(Level& a2, const CompoundTag& tag, DataLoadHelper& a4);
+	virtual bool save(CompoundTag& tag) const;
+	virtual bool saveItemInstanceData(CompoundTag&) const;
+	virtual void setUgcStrings(CompoundTag&, const std::vector<std::string>&) const;
+	virtual void loadBlockData(const CompoundTag& a2, BlockSource& a3, DataLoadHelper& a4);
+	virtual void onCustomTagLoadDone(BlockSource& a2);
+	virtual void tick(BlockSource& region);
+	virtual void onChanged(BlockSource& a2);
+	virtual bool isMovable(BlockSource& a2);
+	virtual bool isCustomNameSaved();
+	virtual void onPlace(BlockSource& a2);
+	virtual void _unknown_12();
+	virtual void onRemoved(BlockSource& a2);
+	virtual bool isPreserved(BlockSource& a2) const;
+	virtual bool shouldPreserve(BlockSource& a2);
+	virtual void triggerEvent(int a2, int a3);
+	virtual void clearCache();
+	virtual void _unknown_18();
+	virtual float getShadowRadius(BlockSource& a2) const;
+	virtual bool hasAlphaLayer() const;
+	virtual BlockActor* getCrackEntity(BlockSource& a2, const BlockPos& a3);
+	virtual AABB getCollisionShape(const IConstBlockSource& a3) const;
+	virtual void getDebugText(std::vector<std::string>& outputInfo, const BlockPos& debugPos);
+	virtual const std::string& getCustomName() const;
+	virtual const std::string& getFilteredCustomName(const gsl::not_null<Bedrock::NonOwnerPointer<UIProfanityContext>>&);
+	virtual std::string getName() const;
+	virtual void setCustomName(const std::string& name);
+	virtual std::string getImmersiveReaderText(BlockSource&);
+	virtual int getRepairCost() const;
+	virtual void _unknown_30();
+	virtual void _unknown_31();
+	virtual void _unknown_32();
+	virtual void _unknown_33();
+	virtual void eraseLootTable();
+	virtual void _unknown_35();
+	virtual void _unknown_36();
+	virtual std::vector<std::string> getUgcStrings(const CompoundTag&) const;
+	virtual void _unknown_38();
+	virtual void _unknown_39();
+
+protected:
+	virtual std::unique_ptr<BlockActorDataPacket> _getUpdatePacket(BlockSource& a2);
+	virtual void _onUpdatePacket(const CompoundTag& a2, BlockSource& a3);
+	virtual bool _playerCanUpdate(const Player& fromPlayer) const;
+};
+
+static_assert(sizeof(BlockActor) == 200);
