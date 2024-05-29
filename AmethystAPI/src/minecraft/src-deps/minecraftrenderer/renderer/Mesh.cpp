9f0b77973b6299788947a3844e43d43784968942
Adrian <adrian.olbrich1@gmail.com>
2024-05-29 19:56:58 +0200

    add rendermesh with texture

--- AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.cpp
+++ AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.cpp
@@ -3,7 +3,14 @@
 
 void mce::Mesh::renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr *materialPtr) {
     // 1.20.71.1
-    using function = decltype(&mce::Mesh::renderMesh);
+    using function = void(mce::Mesh::*)(mce::MeshContext*, const mce::MaterialPtr*);
     static auto func = std::bit_cast<function>(SigScan("48 89 5C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 49 8B D8 C6 44 24")); 
     (this->*func)(meshContext, materialPtr);
+}
+
+void mce::Mesh::renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr* materialPtr, const std::variant<std::monostate, mce::TexturePtr>* texture){
+    // 1.20.72.1 - found by xref mce::Mesh::_renderMesh()
+    using function = void(mce::Mesh::*)(mce::MeshContext*, const mce::MaterialPtr*, const std::variant<std::monostate, mce::TexturePtr>*);
+    static auto func = std::bit_cast<function>(SigScan("40 53 56 57 48 81 EC ? ? ? ? 48 8B D9 C6 44 24"));
+    (this->*func)(meshContext, materialPtr, texture);
 }
--- AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.hpp
+++ AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.hpp
@@ -1,8 +1,10 @@
 #pragma once
-#include <cstdint>
-#include <cstddef>
-#include "minecraft/src-deps/minecraftrenderer/renderer/MeshContext.hpp"
 #include "minecraft/src-deps/minecraftrenderer/renderer/MaterialPtr.hpp"
+#include "minecraft/src-deps/minecraftrenderer/renderer/MeshContext.hpp"
+#include "minecraft/src-client/common/client/renderer/TexturePtr.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <variant>
 
 namespace mce {
     class Mesh {
@@ -12,6 +14,7 @@ namespace mce {
     public:
         // 1.20.51.1 - 48 89 5C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 49 8B D8 C6 44 24
         void renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr* materialPtr);
+        void renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr* materialPtr, const std::variant<std::monostate, mce::TexturePtr>* texture);
     };
 
     static_assert(sizeof(Mesh) == 536);
