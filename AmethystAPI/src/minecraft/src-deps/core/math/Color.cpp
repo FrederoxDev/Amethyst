7e5acefe690b0d1f0f44230ca23d68192c9487f8
Adrian <adrian.olbrich1@gmail.com>
2024-06-16 15:21:16 +0200

    add color

--- AmethystAPI/src/minecraft/src-deps/core/math/Color.cpp
+++ AmethystAPI/src/minecraft/src-deps/core/math/Color.cpp
@@ -1,3 +1,8 @@
 #include "minecraft/src-deps/core/math/Color.hpp"
 
 const mce::Color mce::Color::WHITE(1.0f, 1.0f, 1.0f, 1.0f);
+const mce::Color mce::Color::BLACK(0.0f, 0.0f, 0.0f, 1.0f);
+
+const mce::Color mce::Color::RED(1.0f, 0.0f, 0.0f, 1.0f);
+const mce::Color mce::Color::GREEN(0.0f, 1.0f, 0.0f, 1.0f);
+const mce::Color mce::Color::BLUE(0.0f, 0.0f, 1.0f, 1.0f);
--- AmethystAPI/src/minecraft/src-deps/core/math/Color.hpp
+++ AmethystAPI/src/minecraft/src-deps/core/math/Color.hpp
@@ -25,5 +25,10 @@ public:
     }
 
     static const Color WHITE;
+    static const Color BLACK;
+
+    static const Color RED;
+    static const Color GREEN;
+    static const Color BLUE;
 };
 }; // namespace mce
