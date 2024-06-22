72e1c8a9ff847fb99a9d0571fdb8d1597de40105
Raony Reis <75029692+raonygamer@users.noreply.github.com>
2024-06-21 22:22:17 -0300

    Added public: and dtor on ActorBlock.
    Added public: on the constructor and also added the default virtual dtor.

--- AmethystAPI/src/minecraft/src/common/world/level/block/ActorBlock.hpp
+++ AmethystAPI/src/minecraft/src/common/world/level/block/ActorBlock.hpp
@@ -3,7 +3,10 @@
 
 #pragma pack(push, 1)
 class ActorBlock : public BlockLegacy {
+public:
     ActorBlock(const std::string& nameId, short id, const Material& material) : BlockLegacy(nameId, id, material) {}
+    virtual ~ActorBlock() = default;
+    
 public:
     void neighborChanged(BlockSource& region, const BlockPos& pos, const BlockPos& neighborPos) const override;
     void triggerEvent(BlockSource& region, const BlockPos& pos, int b0, int b1) const override;
