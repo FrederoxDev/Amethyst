7bf3880d398e113bbb3a19ca8d1a9f370bf79aee
Raony Reis <75029692+raonygamer@users.noreply.github.com>
2024-06-21 21:20:53 -0300

    Added ActorBlock class with implementation.
    Nothing to document here...

---
+++ AmethystAPI/src/minecraft/src/common/world/level/block/ActorBlock.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "BlockLegacy.hpp"
+
+#pragma pack(push, 1)
+class ActorBlock : public BlockLegacy {
+    ActorBlock(const std::string& nameId, short id, const Material& material) : BlockLegacy(nameId, id, material) {}
+public:
+    void neighborChanged(BlockSource& region, const BlockPos& pos, const BlockPos& neighborPos) const override;
+    void triggerEvent(BlockSource& region, const BlockPos& pos, int b0, int b1) const override;
+};
+#pragma pack(pop)
