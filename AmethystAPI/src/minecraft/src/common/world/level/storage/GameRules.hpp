29e84fad059cf29c16719e8d7035a3ca151aefb7
Tallis Larsen <tallisblarsen@gmail.com>
2024-06-22 22:53:46 -0500

    Added Floppy's enum that he made because why not? he did it for me anyway

--- AmethystAPI/src/minecraft/src/common/world/level/storage/GameRules.hpp
+++ AmethystAPI/src/minecraft/src/common/world/level/storage/GameRules.hpp
@@ -1,6 +1,46 @@
 #pragma once
 #include <amethyst/Memory.hpp>
 
+enum GameRulesIndex {
+    CommandBlockOutput = 0,
+    DoDayLightCycle = 1,
+    DoEntityDrops = 2,
+    DoFireTick = 3,
+    RecipesUnlock = 4,
+    DoLimitedCrafting = 5,
+    DoMobLoot = 6,
+    DoMobSpawning = 7,
+    DoTileDrops = 8,
+    DoWeatherCycle = 9,
+    DrowningDamage = 10,
+    FallDamage = 11,
+    FireDamage = 12,
+    KeepInventory = 13,
+    MobGriefing = 14,
+    PvP = 15,
+    ShowCoordinates = 16,
+    ShowDaysPlayed = 17,
+    NaturalRegeneration = 18,
+    TntExplodes = 19,
+    SendCommandFeedback = 20,
+    MaxCommandChainLength = 21,
+    DoInsomnia = 22,
+    CommandBlocksEnabled = 23,
+    RandomTickSpeed = 24,
+    DoImmediateRespawn = 25,
+    ShowDeathMessages = 26,
+    FunctionCommandLimit = 27,
+    SpawnRadius = 28,
+    ShowTags = 29,
+    FreezeDamage = 30,
+    RespawnBlocksExplode = 31,
+    ShowBorderEffect = 32,
+    ShowRecipeMessages = 33,
+    PlayersSleepingPercentage = 34,
+    ProjectilesCanBreakBlocks = 35,
+    TntExplosionDropDecay = 36,
+};
+
 class GameRule {
 public:
     /* this + 0 */ std::byte padding0[8];
