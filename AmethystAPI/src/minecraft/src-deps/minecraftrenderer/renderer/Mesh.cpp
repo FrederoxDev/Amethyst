07ae0c1402c45f369a2f09d4befa6a8888fca728
Adrian <adrian.olbrich1@gmail.com>
2024-06-28 17:25:49 +0200

    made impl consistent Attempt #3

--- AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.cpp
+++ AmethystAPI/src/minecraft/src-deps/minecraftrenderer/renderer/Mesh.cpp
@@ -11,7 +11,7 @@ void mce::Mesh::renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr
 void mce::Mesh::renderMesh(mce::MeshContext* meshContext, const mce::MaterialPtr* materialPtr, const std::variant<std::monostate, mce::TexturePtr, mce::ClientTexture, mce::ServerTexture>& texture)
 {
     // 1.20.72.1 - found by xref mce::Mesh::_renderMesh()
-    using function = void(mce::Mesh::*)(mce::MeshContext*, const mce::MaterialPtr&, const std::variant<std::monostate, mce::TexturePtr, mce::ClientTexture, mce::ServerTexture>&);
+    using function = void(mce::Mesh::*)(mce::MeshContext*, const mce::MaterialPtr*, const std::variant<std::monostate, mce::TexturePtr, mce::ClientTexture, mce::ServerTexture>&);
     static auto func = std::bit_cast<function>(SigScan("40 53 56 57 48 81 EC ? ? ? ? 48 8B D9 C6 44 24"));
     (this->*func)(meshContext, materialPtr, texture);
 }
